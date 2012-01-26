20907c098356d1b1c5c1f228ba7c6ef938644a87
Chris Starke <chris@youeni.com>
2012-01-27 08:38:23 +1100

    first import of rails code

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,62 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.limit(5).all
+    end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  # GET /todos/1/edit
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  # POST /todos
+  # POST /todos.json
+  def create
+    @todo = Todo.new(params[:todo])
+
+    respond_to do |format|
+      if @todo.save
+        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.json { render json: @todo, status: :created, location: @todo }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @todo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /todos/1
+  # PUT /todos/1.json
+  def update
+    @todo = Todo.find(params[:id])
+
+    respond_to do |format|
+      if @todo.update_attributes(params[:todo])
+        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @todo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /todos/1
+  # DELETE /todos/1.json
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+
+    respond_to do |format|
+      format.html { redirect_to todos_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TODO::Application.config.secret_token = 'cbb50ca362751f3f168a56396b727170221b5cd09fba208ef0b33defa489d591a6dc1c64bc0c4fcc016b4c50d71865598599e8ef2ec4fbdd9890ac7517bea6c4'
